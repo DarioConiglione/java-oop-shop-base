f66eab271284171b3f5c764d71f59836df9ede80
DarioConiglione <dariocng13@gmail.com>
2026-02-25 18:24:50 +0100

    Add metods

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -8,13 +8,24 @@ public class Prodotto {
     double prezzo;
     double iva;
 
-    Prodotto(int codice, String nome, String descrizione, double prezzo, double iva) {
+    public Prodotto(int codice, String nome, String descrizione, double prezzo, double iva) {
         this.codice = codice;
         this.nome = nome;
         this.descrizione = descrizione;
         this.prezzo = prezzo;
         this.iva = iva;
+    }
 
+    public double PrezzoProdotto() {
+        return prezzo;
+    }
+
+    public double PrezzoConIva() {
+        return prezzo + (prezzo * 0.22);
+    }
+
+    public String NomeCompleto() {
+        return codice + nome;
     }
 
 }
